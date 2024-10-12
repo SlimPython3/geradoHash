cbebf81c5c8a355d6ea7f1e8c41ae8667eca79aa
SlimPython3 <5l1mm4rsh4ll87@gmail.com>
2024-10-11 21:58:23 -0300

    geradoHashAdd12102024

---
+++ geradoHash.py
@@ -0,0 +1,24 @@
+
+import hashlib
+print("Gere um Hash a partir de uma frase ou senha.  :")
+print()
+try:
+	txt = input("Informe :")
+	print("Escolha o Tipo de Hash")
+	menu = int(input("""[*] Hash
+
+		1) md5
+		2) sha1
+
+	"""))
+	if menu not in [1,2]:
+		print("Informe uma das Opçoes :")
+	elif menu == 1:
+		txtencriptado = hashlib.md5(txt.encode('utf-8'))
+		print("A Hash Gerada e: ",txtencriptado.hexdigest())
+	else:
+		txtencriptado = hashlib.sha1(txt.encode('utf-8'))
+		print("A Hash Gerada e: ",txtencriptado.hexdigest())
+except Exception as erro:
+		print(f"Você não informou uma das opções")
+
